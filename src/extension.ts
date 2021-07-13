1ecd8f77f34c45cb0d976b8ad198b1afdd9a3f60
Harish Navnit <harishnavnit@gmail.com>
2021-07-13 12:33:40 +0530

    Fix quickpick

--- src/extension.ts
+++ src/extension.ts
@@ -6,7 +6,7 @@ function colorThemes() {
     return vscode.extensions.all
         .filter(ext => ext.packageJSON.contributes?.themes)
         .map(ext => ext.packageJSON.contributes.themes
-            .map((theme: Object) =>
+            .map((theme: { id: any; label: any; }) =>
                 Object({ 'id': theme.id, 'label': theme.label })
             )
         )
@@ -16,18 +16,20 @@ function colorThemes() {
 
 export function activate(context: vscode.ExtensionContext) {
     let show = vscode.commands.registerCommand('miser.themes', () => {
-        vscode.window.showQuickPick(colorThemes(), {
-            onDidSelectItem: async (item: vscode.Event<void>) => {
-                const stateFile = item.label + '.json';
-                const edit = new vscode.WorkspaceEdit();
-                const uri = vscode.Uri.joinPath(
-                    context.globalStorageUri, 'customizations', stateFile);
+        const picker = vscode.window.createQuickPick();
+        picker.items = colorThemes();
+        picker.onDidAccept(async () => {
+            const item = picker.activeItems[0];
+            const themeFile = `${item.label}.jsonc`;
+            const edit = new vscode.WorkspaceEdit();
+            const uri = vscode.Uri.joinPath(
+                context.globalStorageUri, 'customizations', themeFile);
 
-                edit.createFile(uri, { ignoreIfExists: true, overwrite: false });
-                await vscode.workspace.applyEdit(edit);
-                await vscode.window.showTextDocument(uri, { preview: false });
-            }
-        });	
+            edit.createFile(uri, { ignoreIfExists: true, overwrite: false });
+            await vscode.workspace.applyEdit(edit);
+            await vscode.window.showTextDocument(uri);
+        });
+        picker.show();
     });
 
     context.subscriptions.push(show);
